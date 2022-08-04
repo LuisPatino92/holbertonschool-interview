a15125a8e5af5b765beb288336e03f18cb28ba29
LuisPatino92 <2450@holbertonschool.com>
2022-08-04 16:45:13 -0500

    docstring II

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,6 +3,16 @@
 
 
 def canUnlockAll(boxes):
+    """You have `n` number of locked boxes in front of you.
+    Each box is numbered sequentially from `0` to `n - 1`
+    and each box may contain keychain to the other boxes.
+    This function determines if all the boxes can be opened.
+    * The first box boxes[0] is unlocked.
+    Args:
+        boxes (list): list of list containing keychain
+    Returns:
+        Bool: True if all boxes can be unlocked. False if fail
+    """
     amount_of_boxes = len(boxes)
     hash_dict = {}
     [*map(lambda x: hash_dict.update({x: x}), boxes[0])]
