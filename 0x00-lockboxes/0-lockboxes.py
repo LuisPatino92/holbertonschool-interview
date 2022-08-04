6644e7c155435ee3352ad73fd2fc818dd1f74bbd
LuisPatino92 <2450@holbertonschool.com>
2022-08-04 17:03:00 -0500

    Brute force try

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -15,11 +15,14 @@ def canUnlockAll(boxes):
     hash_dict = integrate_box(boxes[0], hash_dict)
 
     for i in range(1, amount_of_boxes):
+
         if i not in hash_dict.keys():
             return False
         hash_dict = integrate_box(boxes[i], hash_dict)
+        for j in hash_dict.copy().keys():
+            hash_dict = integrate_box(boxes[j], hash_dict)
     return True
 
 
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
