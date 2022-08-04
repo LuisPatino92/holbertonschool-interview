99784d2843f204a6ea1a1812e2159a2aef87ff8f
LuisPatino92 <2450@holbertonschool.com>
2022-08-04 16:24:43 -0500

    First attempt

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    amount_of_boxes = len(boxes)
+    hash_dict = {}
+    [*map(lambda x: hash_dict.update({x: x}), boxes[0])]
+
+    for i in range(1, amount_of_boxes):
+        if i not in hash_dict.keys():
+            return False
+        [*map(lambda x: hash_dict.update({x: x}), boxes[i])]
+    return True
