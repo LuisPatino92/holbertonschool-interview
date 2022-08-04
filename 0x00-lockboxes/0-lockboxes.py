ff811d39bb11e0d8ba12e16a481a14a7206079ac
LuisPatino92 <2450@holbertonschool.com>
2022-08-04 17:07:16 -0500

    CHMOD

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,9 +4,14 @@
 
 def canUnlockAll(boxes):
     """
-    Interview like excercise
+    - boxes is a list of lists
+    - A key with the same number as a box opens that box
+    - You can assume all keys will be positive integers
+    - The first box boxes[0] is unlocked
+    - Return True if all boxes can be opened, else return False
     """
     def integrate_box(box, hash):
+        """ Auxiliar function """
         [*map(lambda x: hash.update({x: x}), box)]
         return hash
 
