17a9ab6a380c9596d136f274ce02fa19f0040fd2
LuisPatino92 <2450@holbertonschool.com>
2022-08-10 09:12:27 -0500

    New approach III

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,22 +8,22 @@ def canUnlockAll(boxes):
     """
 
     hash_dict = {0: True}
-    n_boxes = len(boxes)
+    amount_boxes = len(boxes)
     while (True):
 
-        amount_keys = len(keys)
+        amount_keys = len(hash_dict)
 
         for i in range(len(boxes)):
             if boxes[i] and hash_dict.get(i, False):
                 for j in boxes[i]:
-                    if j < n_boxes:
+                    if j < amount_boxes:
                         hash_dict[j] = True
                     boxes[i] = None
 
-        if not (len(hash_dict) > amount_keys):
+        if len(hash_dict) < amount_keys:
             break
 
-    if amount_keys == len(boxes):
+    if amount_keys == amount_boxes:
         return True
 
     return False
