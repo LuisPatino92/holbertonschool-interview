80263876aaaa90c321ed5fee8e46e3c577ea10c5
LuisPatino92 <2450@holbertonschool.com>
2022-08-22 18:03:29 -0500

    issue with the police

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *n = NULL;
 
 	n = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	if (!n)
 		return (NULL);
 
 	n->parent = parent;
