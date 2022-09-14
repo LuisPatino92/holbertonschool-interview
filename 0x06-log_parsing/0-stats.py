730e444d37eef11e4023214ba83342c7da05c7f9
Luis Patino <luapatinoar@unal.edu.co>
2022-09-14 18:08:09 -0500

    pep8 fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,6 +6,7 @@ Metrics printer
 import signal
 import sys
 
+
 def print_metrics():
     print("File size: {}".format(total_size))
     for metric in sorted(counter.keys()):
@@ -29,7 +30,7 @@ for lines in sys.stdin:
         count += 1
         parsed_line = int(lines.split(" ")[7])
         total_size += int(lines.split(" ")[8])
-        
+
         if parsed_line in counter.keys():
             counter[parsed_line] += 1
         else:
